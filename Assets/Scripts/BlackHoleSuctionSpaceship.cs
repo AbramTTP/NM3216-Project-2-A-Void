bf4142c9f3cb96f28376e0a45187d3178d32dc01
Tan Tack Poh <colonel.tan911220@gmail.com>
2015-10-28 05:21:31 +0800

    Asset Change

--- Assets/Scripts/BlackHoleSuctionSpaceship.cs
+++ Assets/Scripts/BlackHoleSuctionSpaceship.cs
@@ -9,6 +9,9 @@ public class BlackHoleSuctionSpaceship : MonoBehaviour {
 
     public float gravityIncPerDelay = 2.0f;
     public float gravityMaxInGame = 60.0f;
+    public float blackHoleSizeIncPerDelay = 0.2f;
+    public float blackHoleScaleMaxInGame = 1.5f;
+
 
     void start(){
     }
@@ -16,12 +19,20 @@ public class BlackHoleSuctionSpaceship : MonoBehaviour {
     void FixedUpdate()
     {
 
+        blackHole = GameObject.Find("Black Hole");
+        Vector3 blackHoleSize = new Vector3(0f,0f,0f);
+        blackHoleSize = blackHole.transform.localScale;
+
         if ((blackHole.transform.position - transform.position).sqrMagnitude <= 300.0f)
             GetComponent<Rigidbody2D>().AddForce((blackHole.transform.position - transform.position) * GetComponent<Rigidbody2D>().mass * gravityFactor / (blackHole.transform.position - transform.position).sqrMagnitude);
 
         if (Time.timeSinceLevelLoad  % delay == 0.0f && Time.timeSinceLevelLoad  != 0.0f)
         {
             gravityFactor = gravityFactor + gravityIncPerDelay;
+            if(blackHoleSize.x < 1.5f){
+                blackHoleSize = blackHoleSize + (new Vector3(1f, 1f, 1f) * blackHoleSizeIncPerDelay);
+                blackHole.transform.localScale = new Vector3(blackHoleSize.x, blackHoleSize.y, blackHoleSize.z);
+            }
         }
 
         if (gravityFactor > gravityMaxInGame)
